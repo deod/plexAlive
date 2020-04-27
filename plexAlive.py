f511fe276a67fcaefeaab8d416b9a2d25613b327
deod <ameyadkr@gmail.com>
2020-04-27 16:04:53 -0700

    added try blocks to prevent script from crashing.
    Also added rebooting of wifi if server goes down.
    error handling if text cannot be sent

--- plexAlive.py
+++ plexAlive.py
@@ -21,44 +21,72 @@ sleepTime = 180
 # function for sending text message
 def send(message):
     # Replace the number with your own, or consider using an argument\dict for multiple people.
-    to_number = '***PHONE NUMBER***{}'.format(carriers['att'])
+    to_number = '***PHONE NUMBER8***{}'.format(carriers['att'])
     auth = ('***EMAIL***@gmail.com', '***PASSWORD***')
 
     # Establish a secure session with gmail's outgoing SMTP server using your gmail account
-    server = smtplib.SMTP( "smtp.gmail.com", 587)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(auth[0], auth[1])
 
     # Send text message through SMS gateway of destination number
     server.sendmail( auth[0], to_number, message)
 
+# bring plex back up
+def restartPlex():
+    # restart wifi
+    logging.info("Restarting Wi-Fi")
+    subprocess.run(["networksetup","-setairportpower", "en1", "off"])
+    time.sleep(2)
+    subprocess.run(["networksetup","-setairportpower", "en1", "on"])
+    time.sleep(2)
+    # kill plex srever process if running
+    logging.info("Restarting Plex Server App")
+    subprocess.run(["killall", "\"Plex Media Server\""])
+    # restart plex server
+    subprocess.getoutput("open ~/Applications/Plex\\ Media\\ Server.app/")
 
 def checkAlive():
-    server = "***PLEX SERVER IP ADDRESS***"
+    server = "***SERVER IP***"
     port = 32400
     global isUp
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    currDateTime = datetime.now().__str__()
 
     logging.basicConfig(filename='PlexServerCheck.log', level=logging.DEBUG)
+    try:
+        result = sock.connect_ex((server, port))
+        # if server is up
+        if result == 0:
+            message = "Plex Server is alive: " + currDateTime
+            logging.info(message)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((server, port))
-    if result == 0:
-        if isUp == False:
-            send("Your Plex server is back up")
-            isUp = True
-        message = "Plex Server is alive: " + datetime.now().__str__()
-        logging.info(message)
-    else:
-        if isUp == True:
-            send("Your Plex server is down")
-            isUp = False
-        message = "Plex Server down at: " + datetime.now().__str__() + " Attempting to restart"
-        logging.warning(message)
-        # kill plex srever process if running
-        subprocess.run(["killall", "\"Plex Media Server\""])
-        # restart plex server
-        subprocess.getoutput("open ~/Applications/Plex\\ Media\\ Server.app/")
-    sock.close()
+            if isUp == False:
+                try:
+                    send("Your Plex server is back up")
+                except:
+                    logging.error("Unable to send text message, check connectivity")
+                finally:
+                    isUp = True
+        # if server is down
+        else:
+            message = "Plex Server down at: " + currDateTime + " Attempting to restart"
+            logging.warning(message)
+
+            restartPlex()
+
+            if isUp == True:
+                try:
+                    send("Your Plex server is down")
+                except:
+                    logging.error("Unable to send text message, check connectivity")
+                finally:
+                    isUp = False
+    except:
+        logging.warning("Something when wrong checking server status at " + server + " at port "
+                        + port.__str__() + " at " + currDateTime)
+    finally:
+        sock.close()
 
 
 while True:
